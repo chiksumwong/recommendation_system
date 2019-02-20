ea7d30fbfd3f2d0e5b99b10df0dfe0bf9fcffde0
CHIK SUM <chiksum.wong@gmail.com>
2019-02-20 16:37:23 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Request
+
+Python: 3.6
+
+```sh
+$ pip install numpy
+$ pip install scikit-surprise
+```
---
+++ RequestProcess.py
@@ -0,0 +1,94 @@
+import os
+import csv
+import glob
+import time
+import shutil
+
+from surprise import SVD
+from surprise import Dataset, Reader
+from surprise import accuracy
+from surprise.model_selection import train_test_split
+from collections import defaultdict
+
+# get the oldest "RR" file path and name
+filePath = sorted(glob.glob(r"input/RR*"), reverse=False)[0]
+fileName = os.path.basename(filePath)
+
+timestamp = time.strftime("%H:%M:%S")
+print(timestamp + " Request processing start: " + fileName)
+
+# get the userID from "RR" file
+with open(filePath, 'r') as f:
+    rows = csv.reader(f)
+    row = next(rows)
+    inputID = row[0]
+    print(inputID)
+
+
+# print the result from predictions
+def get_items_recommendation_per_user(predictions, maxOutputItems=10):
+    # First map the predictions to each user.
+    top_n = defaultdict(list)
+    for userID, itemID, true_r, est, _ in predictions:
+        top_n[userID].append((itemID, est))
+
+    # Then sort the predictions for each user and retrieve the k highest ones.
+    for userID, user_ratings in top_n.items():
+        user_ratings.sort(key=lambda x: x[1], reverse=True)
+        top_n[userID] = user_ratings[:maxOutputItems]
+        
+    # Print the recommended items for each user
+    for userID, user_ratings in top_n.items():
+#         print(userID, [itemID for (itemID, _) in user_ratings])
+        if inputID == userID:
+            outputRR = ','.join(str(e[0]) for e in user_ratings)
+#             print(outputRR)
+
+    return outputRR
+
+
+
+# create the output file
+def output_RR(inputID, outputRR):
+    timestamp = time.strftime("%Y%m%d%H%M%S")
+    fileName = "output/RR" + timestamp + "out.csv"
+
+    with open(fileName, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([inputID, outputRR])
+
+
+# remove the file from input folder to processed folder
+def move_file(fileName):
+    inputPath = "input/" + fileName
+    processedPath = "processed/" + fileName
+    shutil.move(inputPath, processedPath)
+
+
+# get recommendation
+try:
+    file_path = os.path.expanduser('processed/currentModel.csv')
+    reader = Reader(line_format='user item rating', sep=',')
+    data = Dataset.load_from_file(file_path, reader=reader)
+    trainset = data.build_full_trainset()
+
+    # chose the algo
+    algo = SVD()
+    # train the model
+    algo.fit(trainset)
+
+    # get predict
+    testset = trainset.build_anti_testset()
+    predictions = algo.test(testset)
+    outputRR = get_items_recommendation_per_user(predictions, maxOutputItems=10)
+    
+    # output result
+    output_RR(inputID, outputRR)
+    
+    # move file
+    move_file(fileName)
+except FileNotFoundError:
+    print("Current Model is not exist. Please put the UP file to input folder !") 
+
+timestamp = time.strftime("%H:%M:%S")
+print(timestamp + " Request processing end: " + fileName)
