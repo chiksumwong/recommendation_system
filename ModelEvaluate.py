33621dec0685c4a51294a3c69f8a314770001391
CHIK SUM <chiksum.wong@gmail.com>
2019-02-22 18:05:40 +0800

    Change algo from SVD to SVD++

---
+++ ModelEvaluate.py
@@ -0,0 +1,30 @@
+
+import os
+from surprise import Dataset, Reader
+from surprise import SVD
+from surprise import SVDpp
+from surprise import NMF
+
+from surprise.model_selection import cross_validate
+
+try:
+    file_path = os.path.expanduser('processed/currentModel.csv')
+    reader = Reader(line_format='user item rating', sep=',')
+    data = Dataset.load_from_file(file_path, reader=reader)
+    # chose the algo
+    algo = SVD()
+
+    # Run 5-fold cross-validation and print results.
+    # cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+    cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+    # Root-Mean-Square Error
+    # Mean Absolute Deviation
+
+    algo = SVDpp()
+    cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+
+    algo = NMF()
+    cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+
+except FileNotFoundError:
+    print("Current Model is not exist. Please put the UP file to input folder !")
